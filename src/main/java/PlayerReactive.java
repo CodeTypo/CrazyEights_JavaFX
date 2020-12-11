f4219f782b83c0508fe97fea823b4589a6b4d5df
mars-low <53299913+mars-low@users.noreply.github.com>
2020-12-11 22:25:33 +0100

    add observable fields to player

--- src/main/java/PlayerReactive.java
+++ src/main/java/PlayerReactive.java
@@ -1,3 +1,20 @@
+import javafx.beans.property.SimpleObjectProperty;
+import javafx.beans.value.ObservableObjectValue;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+
 public class PlayerReactive {
-    //to do
+    /**
+     * We need to track player's cards to update them on screen.
+     * Selected cards will be marked in Card's selected field.
+     */
+    private ObservableList<Card> cards = FXCollections.observableArrayList();
+
+    /**
+     * We need to track actual suit, because when crazy eight
+     * is selected, player can select appropriate symbol which
+     * can be somewhat updated on screen.
+     */
+    private ObservableObjectValue<Suit> suit = new SimpleObjectProperty<>();
+
 }
