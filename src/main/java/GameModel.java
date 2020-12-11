330da7ab65aabfe2afcec3c2e4eb4f9bb12723ec
Kuba Skurjat <jakubskurjat99@gmail.com>
2020-12-11 10:05:41 +0100

    First seven tests for GameModel class

--- src/main/java/GameModel.java
+++ src/main/java/GameModel.java
@@ -1,6 +1,5 @@
 import java.util.ArrayList;
 import java.util.Collections;
-import java.util.Iterator;
 import java.util.List;
 import java.util.concurrent.ThreadLocalRandom;
 import java.util.concurrent.atomic.AtomicInteger;
--- src/test/java/GameModelTest.java
+++ src/test/java/GameModelTest.java
@@ -1,29 +1,48 @@
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
-import java.util.Collections;
-import java.util.HashMap;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
 
 class GameModelTest {
     GameModel gameModel = new GameModel();
 
     @Test
-    void prepareCardDeck() {
-        gameModel.prepareCardDeck();
+    void areAllCardsInDeck() {
+        // Arrange
         Set<Card> cardSet = new HashSet<>();
 
+        // Act
+        gameModel.prepareCardDeck();
         cardSet.addAll(gameModel.getStock());
+        int actual = cardSet.size();
 
-        assertEquals(52,cardSet.size());
+        // Assert
+        assertEquals(52,actual);
     }
 
     @Test
-    void invitePlayers() {
+    void areInTheGameAsManyPlayersAsManyCanBeInvited(){
+        // Arrange
+        Player player1 = new Player();
+        Player player2 = new Player();
+        Player player3 = new Player();
+        Player player4 = new Player();
 
+        // Act
+        gameModel.invitePlayers(player1, player2, player3, player4);
+        int actual = gameModel.getPlayers().size();
+
+        // Assert
+        assertEquals(4,actual);
+    }
+
+    @Test
+    void areNoMorePlayersThanSeven() {
+        // Arrange
         Player player1 = new Player();
         Player player2 = new Player();
         Player player3 = new Player();
@@ -32,23 +51,88 @@ class GameModelTest {
         Player player6 = new Player();
         Player player7 = new Player();
         Player player8 = new Player();
-
         gameModel.invitePlayers(player1, player2, player3, player4, player5, player6, player7, player8);
 
+        // Act
+        gameModel.invitePlayers(player1, player2, player3, player4, player5, player6, player7, player8);
+        int actual = gameModel.getPlayers().size();
 
-        assertEquals(7,gameModel.getPlayers().size());
+        // Assert
+        assertEquals(7,actual);
     }
 
     @Test
-    void drawDealer() {
+    void areAtLeastTwoPlayers() {
+        // Arrange
+        Player player1 = new Player();
+
+        // Act
+        gameModel.invitePlayers(player1);
+        int actual = gameModel.getPlayers().size();
+
+        // Assert
+        assertEquals(2,actual);
     }
 
     @Test
-    void beginTheDeal() {
+    void shouldPlayersHaveEightCards_When_InTheGameAreNoMoreThanSixPlayers() {
+        // Arrange
+        Player player1 = new Player();
+        Player player2 = new Player();
+        Player player3 = new Player();
+        Player player4 = new Player();
+
+        // Act
+        gameModel.invitePlayers(player1,player2, player3, player4);
+        gameModel.prepareCardDeck();
+        gameModel.beginTheDeal();
+
+
+        // Assert
+        for(Player players : gameModel.getPlayers())
+            assertEquals(8, players.getCards().size());
     }
 
     @Test
-    void putStarterOnPile() {
+    void shouldPlayersHaveSevenCards_When_InTheGameAreSevenPlayers() {
+        // Arrange
+        Player player1 = new Player();
+        Player player2 = new Player();
+        Player player3 = new Player();
+        Player player4 = new Player();
+        Player player5 = new Player();
+        Player player6 = new Player();
+        Player player7 = new Player();
+
+        // Act
+        gameModel.invitePlayers(player1,player2, player3, player4, player5, player6, player7);
+        gameModel.prepareCardDeck();
+        gameModel.beginTheDeal();
+
+        // Assert
+        for(Player players : gameModel.getPlayers())
+            assertEquals(7, players.getCards().size());
+    }
+
+    @Test
+    void isEightOneOfTheFirstFiveCardsOnThePile() {
+        // Arrange
+        List<Card> stock;
+
+        //Act
+        gameModel.prepareCardDeck();
+        gameModel.putStarterOnPile();
+        stock = gameModel.getStock();
+        int stockSize = stock.size();
+
+        // Assert
+        for(int i=1; i<=5; i++)
+            assertNotEquals(Denomination.EIGHT,stock.get(stockSize-i).getDenomination());
+    }
+
+    @Test
+    void isTheDealerOneOfThePlayers() {
+
     }
 
     @Test
@@ -69,29 +153,5 @@ class GameModelTest {
 
     @Test
     void winnerPoints() {
-    }
-
-    @Test
-    void getPlayers() {
-    }
-
-    @Test
-    void getDealer() {
-    }
-
-    @Test
-    void getTurnPlayer() {
-    }
-
-    @Test
-    void getStock() {
-    }
-
-    @Test
-    void getPile() {
-    }
-
-    @Test
-    void getSuit() {
     }
 }
