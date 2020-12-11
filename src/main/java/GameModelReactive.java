59479b89f4fcbf83aeb90ded1c7ed815a623ef64
mars-low <53299913+mars-low@users.noreply.github.com>
2020-12-11 21:19:21 +0100

    add observable properties

--- src/main/java/GameModelReactive.java
+++ src/main/java/GameModelReactive.java
@@ -1,3 +1,5 @@
+import javafx.beans.property.SimpleBooleanProperty;
+import javafx.beans.property.SimpleIntegerProperty;
 import javafx.beans.property.SimpleObjectProperty;
 import javafx.beans.value.ObservableObjectValue;
 import javafx.collections.FXCollections;
@@ -5,9 +7,29 @@ import javafx.collections.ObservableList;
 
 public class GameModelReactive {
 
-    private ObservableList<Card> cards = FXCollections.observableArrayList();
+    private ObservableList<Card> stock = FXCollections.observableArrayList();
 
+    /**
+     * We need to track number of cards in stock,
+     * because it depends on whether Controller draw stock or not.
+     */
+    private SimpleBooleanProperty isStockEmpty = new SimpleBooleanProperty(stock.isEmpty());
+
+    /**
+     * We need to track turnPlayer because only
+     * he can make action like deal or play card.
+     */
     private ObservableObjectValue<Player> turnPlayer = new SimpleObjectProperty<>();
 
+    /**
+     * We don't need to track all cards in pile.
+     * Only top card is tracked because
+     * Controller needs to refresh its view.
+     */
+    private ObservableObjectValue<Card> topPile = new SimpleObjectProperty<>();
 
+
+    private ObservableObjectValue<Suit> suit = new SimpleObjectProperty<>();
+
+    //Another test with shelves
 }
